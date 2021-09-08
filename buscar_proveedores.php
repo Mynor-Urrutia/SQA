d319aa868a39d645b7c078c7453045af9f4d1092
Mynor-Urrutia <myjourrutia@gmail.com>
2021-09-08 12:45:16 -0600

    Revert "Modificación UI"
    This reverts commit fe6107b3efa860776d2e0c4fe76ba6efb3270f98.

--- buscar_proveedores.php
+++
@@ -1,6 +0,0 @@
-<?php include "templates/header.php"; ?>
-
-
-<a href="index.php">Volver</a>
-
-<?php include "templates/footer.php"; ?>
--- index.php
+++ index.php
@@ -1,33 +1,9 @@
 <?php include "templates/header.php"; ?>
-<h1>Bienvenidos</h1>
-
-<div class="card" style="width: 20rem;">
-    <img src="images/proveedores.png" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Proveedores</h5>
-        <p class="card-text">Modulo de control de proveedores</p>
-    </div>
-    <ul class="list-group list-group-flush card-body">
-        <li class="list-group-item"><a href="ingreso_proveedores.php">Ingresar Proveedores</a></li>
-        <li class="list-group-item"><a href="buscar_proveedores.php">Buscar Proveedores</a></li>
-        <li class="list-group-item"><a href="editar_proveedores.php">Editar Proveedores</a></li>
-        <li class="list-group-item"><a href="borrar_proveedores.php">Borrar Proveedores</a></li>
+    <h1>Bienvenidos</h1>
+    <ul>
+        <li><a href="ingreso_productos.php">Ingresar Productos</a></li>
+        <li><a href="buscar_productos.php">Buscar Productos</a></li>
+        <li><a href="editar_productos.php">Editar Productos</a></li>
+        <li><a href="borrar_productos.php">Borrar Productos</a></li>
     </ul>
-</div>
-
-<div class="card" style="width: 20rem;">
-    <img src="images/proveedores.png" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Proveedores</h5>
-        <p class="card-text">Modulo de control de proveedores</p>
-    </div>
-    <ul class="list-group list-group-flush card-body">
-        <li class="list-group-item"><a href="ingreso_productos.php">Ingresar Productos</a></li>
-        <li class="list-group-item"><a href="buscar_productos.php">Buscar Productos</a></li>
-        <li class="list-group-item"><a href="editar_productos.php">Editar Productos</a></li>
-        <li class="list-group-item"><a href="borrar_productos.php">Borrar Productos</a></li>
-    </ul>
-</div>
-
-
 <?php include "templates/footer.php"; ?>
